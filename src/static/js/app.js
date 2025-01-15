6b41098ae1f57aeb98111a8490ef83f547df2b38
ikuto2002 <nkgw191020@gmail.com>
2025-01-15 23:38:36 +0900

    add the title

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,6 +4,7 @@ function App() {
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
+                    <h>ToDo App by Ikuto Nakagawa</h>
                     <TodoListCard />
                 </Col>
             </Row>
